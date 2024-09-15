2833b6ab440053be0fbcb3893a906876a7460e76
NguyenBao1234 <robertpamvn@gmail.com>
2024-09-15 19:02:02 +0700

    - Fix file path wrong

--- Source/MainProgram.py
+++ Source/MainProgram.py
@@ -1,23 +1,26 @@
 import cv2
-
 print(cv2.__version__)
 print("press Q to quit the program")
 
 """Test Image"""
-QuaBom = cv2.imread('Asset/QuaBom.png')
+QuaBom = cv2.imread('../Asset/QuaBom.png')
+'''if QuaBom is None:
+    print("Error: Image not found or cannot be loaded.")
+    exit()'''
+
 cv2.imshow('AI Thi Giac May Tinh', QuaBom)
 
 (w,h,d)=QuaBom.shape
 print("CR= {}, CD= {}, ChieuSau= {}".format(h,w,d))
 
 """TestVideo"""
-videoCat= cv2.VideoCapture('Asset/HuhCat.mp4')
+videoCat= cv2.VideoCapture('../Asset/HuhCat.mp4')
 while True:
     ret, frame = videoCat.read()
     if not ret:
         break
     cv2.imshow('VideoCat', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        print("Press anykey to confirm...");break
+        print("Press any key to confirm...");break
 
 cv2.waitKey(0)
