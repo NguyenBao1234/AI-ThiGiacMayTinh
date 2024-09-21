64194f3b5a065f67e34834b8a095cb26297fd6be
DuyViet04 <duyviet130904@gmail.com>
2024-09-21 11:19:39 +0700

    capture image 1st option

--- Source/CameraScanning.py
+++ Source/CameraScanning.py
@@ -1,3 +1,4 @@
+import time
 from functools import partial
 
 import cv2
@@ -13,17 +14,17 @@ class ScanningApp(App):
     def build(self):
         self.HUD = FloatLayout()
         self.title = 'Camera Scanning'
-        self.image = Image(allow_stretch=True,keep_ratio=False)
+        self.image = Image(allow_stretch=True, keep_ratio=False)
         self.camera = cv2.VideoCapture(0)
 
         CaptureButton = Button(size_hint=(0.1, 0.1),
                                on_press=self.OnPressCaptureButton,
                                background_normal='../Asset/CaptureNormalButton.png',
-                               background_down ='../Asset/CapturePressButton.png',
-                               border = (3, 3, 3, 3),
+                               background_down='../Asset/CapturePressButton.png',
+                               border=(3, 3, 3, 3),
                                pos_hint={'center_x': 0.5, 'y': 0})
 
-        ''' button data for each dêtcted object '''
+        ''' button data for each detected object '''
         self.object_buttons = {}
 
         """SetUpModel"""
@@ -44,48 +45,73 @@ class ScanningApp(App):
         self.HUD.add_widget(self.image)
         self.HUD.add_widget(CaptureButton)
         return self.HUD
-    #event chup anh
+
+    # event chup anh
     def OnPressCaptureButton(self, instance):
         print("CapturePressButton")
+        ret, frame = self.camera.read()
+
+        if ret:
+            timestr = time.strftime("%Y%m%d-%H%M%S")
+            for classID, confidence, box in zip(*self.Dectec(frame)):
+                self.DrawBoundingBoxes(frame, classID, confidence, box)
+            cv2.imwrite('../ImageCaptured/{}.jpg'.format(timestr), frame)
+
     def OnPressInforBtn(self, instance, ObjectName):
         print("infor", {ObjectName})
 
     """update realtime"""
+
     def update(self, dt):
         ret, frame = self.camera.read()
 
         if ret:
             current_objects = []
-            classIds, confs, bbox = self.net.detect(frame, confThreshold=0.5)
+            #self.Dectec(frame)
+            classIds, confs, bbox = self.net.detect(frame,confThreshold=0.5)
 
             if len(classIds) != 0:
                 for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
                     current_objects.append(tuple(box))
-                    cv2.rectangle(frame, box, (0, 155, 255), 2)
+                    self.DrawBoundingBoxes(frame, classId, confidence, box)
+                    '''cv2.rectangle(frame, box, (0, 155, 255), 2)
                     cv2.putText(frame, self.classNames[classId - 1].upper(), (box[0] + 10, box[1] + 20),
                                 cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), thickness=2)
                     cv2.putText(frame, str(round(confidence * 100, 2)) + '%', (box[0] + 10, box[1] + 40),
-                                cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), thickness=2)
+                                cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), thickness=2)'''
                     """Assign all infor buttons to the dectected object """
                     if tuple(box) not in self.object_buttons:
                         InforBtn = Button(text=self.classNames[classId - 1].upper(), size_hint=(None, None),
-                                          size=(100, 50), pos=(int(box[0]), int(frame.shape[0] - box[1] - 50)) )
-                        InforBtn.bind(on_press = partial (self.OnPressInforBtn, ObjectName = self.classNames[classId - 1]) )
+                                          size=(100, 50), pos=(int(box[0]), int(frame.shape[0] - box[1] - 50)))
+                        InforBtn.bind(on_press=partial(self.OnPressInforBtn, ObjectName=self.classNames[classId - 1]))
                         self.object_buttons[tuple(box)] = InforBtn
                         self.HUD.add_widget(InforBtn)
-                    else :
+                    else:
                         self.object_buttons[tuple(box)].pos = (int(box[0]), int(frame.shape[0] - box[1] - 50))
             """remove the undetected object's info button"""
             for old_box in list(self.object_buttons.keys()):
                 if old_box not in current_objects:
                     self.HUD.remove_widget(self.object_buttons[old_box])
                     del self.object_buttons[old_box]
-            buf = cv2.flip(frame,0).tostring()
+            buf = cv2.flip(frame, 0).tostring()
             image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
             image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
             self.image.texture = image_texture
 
     def on_stop(self):
         self.camera.release()
+
+    def DrawBoundingBoxes(self, image, classId, confidence, box):
+        cv2.rectangle(image, box, (0, 155, 255), 2)
+        cv2.putText(image, self.classNames[classId - 1].upper(), (box[0] + 10, box[1] + 20),
+                    cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), thickness=2)
+        cv2.putText(image, str(round(confidence * 100, 2)) + '%', (box[0] + 10, box[1] + 40),
+                    cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), thickness=2)
+
+    # Using tuple: MyName: data_detect_tuple
+    def Dectec(self, image):
+        classIds, confs, bbox = self.net.detect(image, confThreshold=0.5)
+        return classIds, confs, bbox
+
 
 ScanningApp().run()
